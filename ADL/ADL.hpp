590320b48ac4cc90cb79a6333d1caecee427d0d2
vDrWool <vdrwool@gmail.com>
2022-04-08 12:53:53 +0300

    ADL new example

--- ADL/ADL.hpp
+++ ADL/ADL.hpp
@@ -31,10 +31,30 @@ void foo(S) {}
 } // namespace ns2
 
 void start() {
-    foo(ns1::S()); // ns1::foo
-    foo(ns2::S()); // ns2::foo
+    foo(ns1::S()); // ns1::foo.
+    foo(ns2::S()); // ns2::foo.
 
-    // ns1::foo(S()); // Error: 'S' is not declared in this namespace
-    // ns2::foo(S()); // Error: 'S' is not declared in this namespace
+    // ns1::foo(S()); // Error: 'S' is not declared in this namespace.
+    // ns2::foo(S()); // Error: 'S' is not declared in this namespace.
 }
 } // namespace example_2
+
+
+namespace ns {
+struct S {};
+void foo(S) {}
+} // namespace ns
+
+void foo(ns::S) {}
+
+void start() 
+{ 
+    ns::S s;
+
+    // foo(s); // Error: could be ns::foo or foo. 
+
+    // Resolve.
+    ns::foo(s);
+    ::foo(s); 
+    (foo)(s); // Same as ::foo(s).
+}
--- ADL/ADLMain.cpp
+++ ADL/ADLMain.cpp
@@ -3,6 +3,7 @@
 int main() { 
 	example_1::start();
     example_2::start();
+    ::start(); // example_3
 
 
 	return EXIT_SUCCESS;
